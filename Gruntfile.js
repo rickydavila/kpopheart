f5b8558ce08915bd7f70a53faa5d92c1949ae771
rickydavila <richardadavila@gmail.com>
2013-09-20 17:26:04 -0500

    yay

--- Gruntfile.js
+++ Gruntfile.js
@@ -240,18 +240,19 @@ module.exports = function (grunt) {
     copy: {
       dist: {
         files: [{
-                expand: true,
-                dest: '<%= yeoman.dist %>',
-                cwd: 'heroku',
-                src: '*',
-                rename: function (dest, src) {
-                    var path = require('path');
-                    if (src === 'distpackage.json') {
-                        return path.join(dest, 'package.json');
-                    }
-                    return path.join(dest, src);
-                }
-            }, {
+          expand: true,
+          dest: '<%= yeoman.dist %>',
+          cwd: 'heroku',
+          src: '*',
+          rename: function (dest, src) {
+              var path = require('path');
+              if (src === 'distpackage.json') {
+                return path.join(dest, 'package.json');
+              }
+              return path.join(dest, src);
+            }
+        },
+        {
           expand: true,
           cwd: '.tmp/images',
           dest: '<%= yeoman.dist %>/images',
