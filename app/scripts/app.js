0659b123f6126e34e815863fe2be74684618d150
rickydavila <richardadavila@gmail.com>
2013-09-23 12:58:23 -0500

    before reverting code to something like example

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,7 @@ angular.module('kpopApp', [])
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        controller: 'SwitchableGridController'
       })
       .otherwise({
         redirectTo: '/'
