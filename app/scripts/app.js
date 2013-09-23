d7a2817796ddad898adc9b8bedd4c484544126bf
rickydavila <richardadavila@gmail.com>
2013-09-23 16:33:21 -0500

    before changing bower file to update

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,7 +21,7 @@ app.factory('instagram', function($resource){
       // The ngResource module gives us the $resource service. It makes working with
       // AJAX easy. Here I am using the client_id of a test app. Replace it with yours.
 
-      var api = $resource('https://api.instagram.com/v1/media/popular?client_id=:client_id&callback=JSON_CALLBACK',{
+      var api = $resource('https://api.instagram.com/v1/tags/kpop/media/recent?client_id=:client_id&callback=JSON_CALLBACK',{
         client_id: '642176ece1e7445e99244cec26f4de1f'
       },{
         // This creates an action which we've chosen to name "fetch". It issues
