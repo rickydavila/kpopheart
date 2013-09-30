58c72b520a50627304220acb94093272c9541596
rickydavila <richardadavila@gmail.com>
2013-09-30 13:05:20 -0500

    trying to get font to work in build

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -55,13 +55,11 @@ app.controller('SwitchableGridController', ['$scope', 'instagram', function($sco
 // Assigning the pics array will cause the view
 // to be automatically redrawn by Angular.
         $scope.pics = data;
-        console.log(data);
         var objects = [], index = 0, x = Math.ceil(($scope.pics).length / 3);
-        console.log('this is x: ' + x);
+        //console.log('this is x: ' + x);
         for (var i = 0; i < x; i++) {
           index = i * 3;
           var obj = {i1: ($scope.pics)[index]};
-          console.log(obj);
           if (($scope.pics)[index + 1]) {obj.i2 = ($scope.pics)[index + 1];}
           if (($scope.pics)[index + 2]) {obj.i3 = ($scope.pics)[index + 2];}
           objects.push(obj);
