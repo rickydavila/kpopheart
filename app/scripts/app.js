fab634d0342d7c158c85a8e1236e784f0d11e445
rickydavila <richardadavila@gmail.com>
2013-09-30 12:31:38 -0500

    cleaned up js and added controller to app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -40,3 +40,33 @@ app.factory('instagram', function($resource){
     }
   };
 });
+
+app.controller('SwitchableGridController', ['$scope', 'instagram', function($scope, instagram) {
+// Default layout of the app. Clicking the buttons in the toolbar
+// changes this value.
+    //'use strict';
+    $scope.layout = 'grid';
+    $scope.items = [];
+    $scope.pics = [];
+
+// Use the instagram service and fetch a list of the popular pics
+    instagram.fetchPopular(function (data) {
+
+// Assigning the pics array will cause the view
+// to be automatically redrawn by Angular.
+        $scope.pics = data;
+        console.log(data);
+        var objects = [], index = 0, x = Math.ceil(($scope.pics).length / 3);
+        console.log('this is x: ' + x);
+        for (var i = 0; i < x; i++) {
+          index = i * 3;
+          var obj = {i1: ($scope.pics)[index]};
+          console.log(obj);
+          if (($scope.pics)[index + 1]) {obj.i2 = ($scope.pics)[index + 1];}
+          if (($scope.pics)[index + 2]) {obj.i3 = ($scope.pics)[index + 2];}
+          objects.push(obj);
+        }
+        $scope.items = objects;
+      });
+/*console.log($scope.items); */
+  }]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,32 +1,32 @@
+/*
 'use strict';
-function SwitchableGridController($scope, instagram){
+function SwitchableGridController($scope, instagram) {
 
-	// Default layout of the app. Clicking the buttons in the toolbar
-	// changes this value.
-	//'use strict';
-	$scope.layout = 'grid';
-	$scope.items = [];
-	$scope.pics = [];
+// Default layout of the app. Clicking the buttons in the toolbar
+// changes this value.
+//'use strict';
+    $scope.layout = 'grid';
+    $scope.items = [];
+    $scope.pics = [];
 
-	// Use the instagram service and fetch a list of the popular pics
-	instagram.fetchPopular(function(data){
+// Use the instagram service and fetch a list of the popular pics
+    instagram.fetchPopular(function (data) {
 
-		// Assigning the pics array will cause the view
-		// to be automatically redrawn by Angular.
-		$scope.pics = data;
-		var objects = [];
-	  	var index = 0;
-	 	var x = Math.ceil(($scope.pics).length / 3);
-	 	console.log('this is x: ' + x);
-		for (var i = 0; i < x; i++) {
-		   index = i * 3;
-		   var obj = {i1: ($scope.pics)[index]};
-		   console.log(obj);
-		   if (($scope.pics)[index + 1]) {obj.i2 = ($scope.pics)[index + 1];}
-		   if (($scope.pics)[index + 2]) {obj.i3 = ($scope.pics)[index + 2];}
-		   objects.push(obj);
-		};
-		$scope.items = objects;
-	});
-	/*console.log($scope.items); */
-}
+// Assigning the pics array will cause the view
+// to be automatically redrawn by Angular.
+        $scope.pics = data;
+        var objects = [], index = 0, x = Math.ceil(($scope.pics).length / 3);
+        console.log('this is x: ' + x);
+        for (var i = 0; i < x; i++) {
+          index = i * 3;
+          var obj = {i1: ($scope.pics)[index]};
+          console.log(obj);
+          if (($scope.pics)[index + 1]) {obj.i2 = ($scope.pics)[index + 1];}
+          if (($scope.pics)[index + 2]) {obj.i3 = ($scope.pics)[index + 2];}
+          objects.push(obj);
+        }
+        $scope.items = objects;
+      });
+/*console.log($scope.items);
+  }
+*/
